624092a0b43bc21deaced924a4b4e3b4d5a0a2a3
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-04-18 01:27:05 +0200

    added resume href, to improve navigation logic

--- src/store/nav-items.tsx
+++ src/store/nav-items.tsx
@@ -1,5 +1,14 @@
+import { 
+  BiHomeAlt, 
+  BiUser, 
+  BiFolder, 
+  BiCodeAlt, 
+  BiEnvelope 
+} from 'react-icons/bi'
+import { SiReaddotcv } from "react-icons/si";
+import { MdOutlineRateReview } from "react-icons/md";
 
-export const scrollToSection = (id: string) => {
+const scrollToSection = (id: string) => {
     const section = document.getElementById(id);
     if (section) {
         section.scrollIntoView({ behavior: 'smooth' });
@@ -7,10 +16,11 @@ export const scrollToSection = (id: string) => {
 };
 
 export const navItems = [
-    { label: '#Intro', onClick: () => scrollToSection('hero')},
-    { label: '#About', onClick: () => scrollToSection('about')},
-    { label: '#Projects', onClick: () => scrollToSection('projects')},
-    { label: '#Skills', onClick: () => scrollToSection('skills')},
-    { label: '#Testimonials', onClick: () => scrollToSection('testimonials')},
-    { label: '#Contact', onClick: () => scrollToSection('contact')},
+    { icon: <BiHomeAlt size={18} />, label: 'Intro', onClick: () => scrollToSection('hero')},
+    { icon: <BiUser size={18} />, label: 'About', onClick: () => scrollToSection('about')},
+    { icon: <BiFolder size={18} />, label: 'Projects', onClick: () => scrollToSection('projects')},
+    { icon: <BiCodeAlt size={18} />, label: 'Skills', onClick: () => scrollToSection('skills')},
+    { icon: <MdOutlineRateReview size={18} />, label: 'Testimonials', onClick: () => scrollToSection('testimonials')},
+    { icon: <BiEnvelope size={18} />, label: 'Contact', onClick: () => scrollToSection('contact')},
+    { icon: <SiReaddotcv size={18} color='var(--primary)' />, label: 'Resume', onClick: () => window.location.href = '/resume' },
 ];
