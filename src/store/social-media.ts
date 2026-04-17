5177655d00af4272dade2b0fd700aa5ccab74428
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-04-18 01:31:10 +0200

    added social-media in a seperate file

---
+++ src/store/social-media.ts
@@ -0,0 +1,31 @@
+
+export const socialMedia = [
+    {
+        id: 0,
+        name: "GitHub",
+        designation: "Visit Profile",
+        image: "/skills/github.svg",
+        href: "https://github.com/abdullahMohamed13",
+    },
+    {
+        id: 1,
+        name: "LinkedIn",
+        designation: "View Profile",
+        image: "/contacts/linkedin.svg",
+        href: "https://www.linkedin.com/in/abdallah-m-aziz",
+    },
+    {
+        id: 2,
+        name: "Gmail",
+        designation: "Send Email",
+        image: "/contacts/gmail.svg",
+        href: "mailto:abdullah.229op@gmail.com",
+    },
+    {
+        id: 4,
+        name: "WhatsApp",
+        designation: "Copy To Clipboard",
+        image: "/contacts/whatsapp.webp",
+        href: "https://wa.me/201010434465",
+    },
+];
