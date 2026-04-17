dc5ecc8f21faafe0fd90e38488c4124f2d573d15
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-04-17 20:45:56 +0200

    style: removed icons, added # instead

--- src/store/nav-items.tsx
+++ src/store/nav-items.tsx
@@ -1,11 +1,3 @@
-import { 
-  BiHomeAlt, 
-  BiUser, 
-  BiFolder, 
-  BiChat, 
-  BiCodeAlt, 
-  BiEnvelope 
-} from 'react-icons/bi'
 
 export const scrollToSection = (id: string) => {
     const section = document.getElementById(id);
@@ -15,10 +7,10 @@ export const scrollToSection = (id: string) => {
 };
 
 export const navItems = [
-    { icon: <BiHomeAlt size={18} />, label: 'Intro', onClick: () => scrollToSection('hero')},
-    { icon: <BiUser size={18} />, label: 'About', onClick: () => scrollToSection('about')},
-    { icon: <BiFolder size={18} />, label: 'Projects', onClick: () => scrollToSection('projects')},
-    { icon: <BiCodeAlt size={18} />, label: 'Skills', onClick: () => scrollToSection('skills')},
-    { icon: <BiChat size={18} />, label: 'Testimonials', onClick: () => scrollToSection('testimonials')},
-    { icon: <BiEnvelope size={18} />, label: 'Contact', onClick: () => scrollToSection('contact')},
+    { label: '#Intro', onClick: () => scrollToSection('hero')},
+    { label: '#About', onClick: () => scrollToSection('about')},
+    { label: '#Projects', onClick: () => scrollToSection('projects')},
+    { label: '#Skills', onClick: () => scrollToSection('skills')},
+    { label: '#Testimonials', onClick: () => scrollToSection('testimonials')},
+    { label: '#Contact', onClick: () => scrollToSection('contact')},
 ];
