0e4675838bd4050c41549b6bf6d679e9a9aeac36
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-05-13 06:17:29 +0300

    remove unused hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useEffect } from "react";
+import { lazy, Suspense } from "react";
 // sections
 import Hero from "@/sections/Hero";
 const Projects = lazy(() => import("../sections/Projects"));
